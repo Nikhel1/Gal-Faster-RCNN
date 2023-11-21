9ff09d48cb8e3829595e6c72e5d69b746a529b28
Nikhel <ngupta@usm.lmu.de>
2023-11-21 16:45:25 +0800

    Add files via upload

---
+++ evaluate.py
@@ -0,0 +1,112 @@
+import torch, torchvision
+
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+from detectron2.data.catalog import DatasetCatalog
+
+from detectron2.data.datasets import register_coco_instances
+register_coco_instances("my_dataset_train", {}, "./RadioGalaxyNET_V4/annotations/instances_train2017.json", "./RadioGalaxyNET_V4/train2017")
+register_coco_instances("my_dataset_val", {}, "./RadioGalaxyNET_V4/annotations/instances_val2017.json", "./RadioGalaxyNET_V4/val2017")
+register_coco_instances("my_dataset_test", {}, "./RadioGalaxyNET_V4/annotations/instances_test2017.json", "./RadioGalaxyNET_V4/test2017")
+
+"""# Train Custom Detectron2 Detector"""
+
+# We are importing our own Trainer Module here to use the COCO validation evaluation during training. 
+# Otherwise no validation eval occurs.
+
+from detectron2.engine import DefaultTrainer
+from detectron2.evaluation import COCOEvaluator
+
+class CocoTrainer(DefaultTrainer):
+
+  @classmethod
+  def build_evaluator(cls, cfg, dataset_name, output_folder=None):
+
+    if output_folder is None:
+        os.makedirs("coco_eval", exist_ok=True)
+        output_folder = "coco_eval"
+
+    return COCOEvaluator(dataset_name, cfg, False, output_folder)
+    
+    
+from detectron2.config import get_cfg
+#from detectron2.evaluation.coco_evaluation import COCOEvaluator
+import os
+
+cfg = get_cfg()
+# select from modelzoo here: https://github.com/facebookresearch/detectron2/blob/master/MODEL_ZOO.md#coco-object-detection-baselines
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+cfg.DATASETS.TRAIN = ("my_dataset_train",)
+cfg.DATASETS.TEST = ("my_dataset_test",)
+
+cfg.DATALOADER.NUM_WORKERS = 4
+cfg.MODEL.WEIGHTS = None #model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+cfg.SOLVER.IMS_PER_BATCH = 4
+cfg.SOLVER.BASE_LR = 0.001
+
+
+cfg.SOLVER.WARMUP_ITERS = 1000
+cfg.SOLVER.MAX_ITER = 20000 #adjust up if val mAP is still rising, adjust down if overfit
+cfg.SOLVER.STEPS = (10000, 15000)
+cfg.SOLVER.GAMMA = 0.05
+
+
+cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 64
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 4 #your number of classes 
+
+cfg.TEST.EVAL_PERIOD = 1000
+cfg.OUTPUT_DIR = './outputs'
+
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+trainer = CocoTrainer(cfg)
+trainer.resume_or_load(resume=False)
+#trainer.train()
+
+# Commented out IPython magic to ensure Python compatibility.
+# Look at training curves in tensorboard:
+# %load_ext tensorboard
+# %tensorboard --logdir outputs
+
+#test evaluation
+from detectron2.data import DatasetCatalog, MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0. #0.85
+predictor = DefaultPredictor(cfg)
+evaluator = COCOEvaluator("my_dataset_test", cfg, False, output_dir="./outputs/")
+val_loader = build_detection_test_loader(cfg, "my_dataset_test")
+inference_on_dataset(trainer.model, val_loader, evaluator)
+
+"""# Inference with Detectron2 Saved Weights
+
+changed 
+
+coco_eval.params.areaRng = [[0 ** 2, 1e5 ** 2], [0 ** 2, 24 ** 2], [24 ** 2, 48 ** 2], [48 ** 2, 1e5 ** 2]]
+coco_eval.params.areaRngLbl = ['all', 'small', 'medium', 'large']
+
+in file: /datasets/work/mlaifsp-sa-askap/work/softwares/bracewell/anaconda_bowen/anaconda3/envs/frcnn/lib/python3.9/site-packages/detectron2/evaluation/coco_evaluation.py
+"""
+
+# Commented out IPython magic to ensure Python compatibility.
+# %ls ./output/
+
+cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+cfg.DATASETS.TEST = ("my_dataset_test", )
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
+predictor = DefaultPredictor(cfg)
+test_metadata = MetadataCatalog.get("my_dataset_test")
+
